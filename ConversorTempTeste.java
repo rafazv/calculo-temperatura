be065c8fb47b28369bd10f523f1997215d6a09ed
Rafaela <ral@icomp.ufam.edu.br>
2021-05-27 22:09:52 -0400

    Delete unused imports

--- ConversorTempTeste.java
+++ ConversorTempTeste.java
@@ -1,11 +1,5 @@
 import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class ConversorTempTeste {
 
