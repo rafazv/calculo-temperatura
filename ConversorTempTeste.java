557fff3fc20790579d13f1fd802da9f14dd750b1
Rafaela <ral@icomp.ufam.edu.br>
2021-05-27 22:04:24 -0400

    Add prints on tests

--- ConversorTempTeste.java
+++ ConversorTempTeste.java
@@ -1,6 +1,11 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ConversorTempTeste {
 
@@ -9,12 +14,18 @@ public class ConversorTempTeste {
         ConversorTemp conversor = new ConversorTemp();
         double respMetodo, temp;
 
+        System.out.println("\nFahrenheint to Celsius - TEST #1");
         temp = 32;
+        System.out.println("Fahrenheint temperature: " + temp);
         respMetodo = conversor.fahrenheitCelsius(temp);
+        System.out.println("Celsius temperature: " + respMetodo);
         assertEquals(0.0, respMetodo, 0.0002);
 
+        System.out.println("\nFahrenheint to Celsius - TEST #2");
         temp = 84;
+        System.out.println("Fahrenheint temperature: " + temp);
         respMetodo = conversor.fahrenheitCelsius(temp);
+        System.out.println("Celsius temperature: " + respMetodo);
         assertEquals(28.88889, respMetodo, 0.0002);
     }
 
@@ -23,12 +34,18 @@ public class ConversorTempTeste {
         ConversorTemp conversor = new ConversorTemp();
         double respMetodo, temp;
 
+        System.out.println("\nCelsius to Fahrenheint - TEST #1");
         temp = 0;
+        System.out.println("Celsius temperature: " + temp);
         respMetodo = conversor.celsiusFahrenheit(temp);
+        System.out.println("Fahrenheint temperature: " + respMetodo);
         assertEquals(32.0, respMetodo, 0.0002);
 
+        System.out.println("\nCelsius to Fahrenheint - TEST #2");
         temp = 35.9;
+        System.out.println("Celsius temperature: " + temp);
         respMetodo = conversor.celsiusFahrenheit(temp);
+        System.out.println("Fahrenheint temperature: " + respMetodo);
         assertEquals(96.62, respMetodo, 0.0002);
     }
 }
